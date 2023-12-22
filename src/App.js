500aeee40c062b3a92e002fced718ddb88f27f6e
Akanksha Kriti <akankshakriti09@gmail.com>
2023-12-22 06:24:01 +0530

    List created in Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import Card from './Components/Card/Card';
+// import Card from './Components/Card/Card';
+import List from './Components/List/List';
 
 function App() {
   return (
     <>
       <Navbar />
       <section className="board-details">
-       <Card />
+       {/* <Card /> */}
+       <div className="board-details-list">
+        <List />
+        <List />
+        <List />
+        <List />
+        <List />
+        </div>
       </section>
     </>
   );
