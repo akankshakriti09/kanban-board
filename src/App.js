ef53d94c49e4627ba840043bf8b323b91945a4de
Akanksha Kriti <akankshakriti09@gmail.com>
2023-12-22 06:14:12 +0530

    Card added in Compnents folder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
+import Card from './Components/Card/Card';
 
 function App() {
   return (
     <>
       <Navbar />
       <section className="board-details">
-        
+       <Card />
       </section>
     </>
   );
