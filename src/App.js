738c628064c8779679cdd47886c224aa98602c08
Akanksha Kriti <akankshakriti09@gmail.com>
2023-12-23 22:08:15 +0530

    change in icon

--- src/App.js
+++ src/App.js
@@ -1,87 +1,104 @@
-import React, {useEffect, useState, useCallback} from 'react';
-import axios  from 'axios';
-
-import './App.css';
-import Navbar from './Components/Navbar/Navbar';
-import List from './Components/List/List';
+import React, { useEffect, useState, useCallback } from "react";
+import "./App.css";
+import axios from "axios";
+import Navbar from "./Components/Navbar/Navbar";
+import List from "./Components/List/List";
 
 function App() {
-  const statusList = ['Backlog', 'Todo', 'In progress', 'Done', 'Cancelled']
-  const userList = ['Anoop Sharma', 'Yogesh','Suresh', 'Shankar Kumar', 'Ramesh']
-  const priorityList = [{name:'No priority', priority: 0}, {name:'Low', priority: 1}, {name:'Medium', priority: 2}, {name:'High', priority: 3}, {name:'Urgent', priority: 4}]
+  const [groupValue, setgroupValue] = useState(
+    getStateFromLocalStorage() || "status"
+  );
+  const [orderValue, setorderValue] = useState("title");
 
-  const [groupValue, setgroupValue] = useState(getStateFromLocalStorage() || 'status')
-  const [orderValue, setorderValue] = useState('title')
+  const statusList = ["Backlog", "Todo", "In progress", "Done", "Cancelled"];
+  const [userList, setUserList] = useState([]);
+  const priorityList = [
+    { name: "No priority", priority: 0 },
+    { name: "Low", priority: 1 },
+    { name: "Medium", priority: 2 },
+    { name: "High", priority: 3 },
+    { name: "Urgent", priority: 4 },
+  ];
+
   const [ticketDetails, setticketDetails] = useState([]);
 
-  const orderDataByValue = useCallback(async (cardsArray) => {
-    if (orderValue === 'priority') {
-      cardsArray.sort((a, b) => a.priority - b.priority);
-    } else if (orderValue === 'title') {
-      cardsArray.sort((a, b) => {
-        const titleA = a.title.toLowerCase();
-        const titleB = b.title.toLowerCase();
+  const orderDataByValue = useCallback(
+    async (cardsArray) => {
+      if (orderValue === "priority") {
+        cardsArray.sort((a, b) => a.priority - b.priority);
+      } else if (orderValue === "title") {
+        cardsArray.sort((a, b) => {
+          const titleA = a.title.toLowerCase();
+          const titleB = b.title.toLowerCase();
 
-        if (titleA < titleB) {
-          return -1;
-        } else if (titleA > titleB) {
-          return 1;
-        } else {
-          return 0;
-        }
-      });
-    }
-    await setticketDetails(cardsArray);
-  }, [orderValue, setticketDetails]);
+          if (titleA < titleB) {
+            return -1;
+          } else if (titleA > titleB) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      }
+      await setticketDetails(cardsArray);
+    },
+    [orderValue, setticketDetails]
+  );
 
   function saveStateToLocalStorage(state) {
-    localStorage.setItem('groupValue', JSON.stringify(state));
+    localStorage.setItem("groupValue", JSON.stringify(state));
   }
 
   function getStateFromLocalStorage() {
-    const storedState = localStorage.getItem('groupValue');
+    const storedState = localStorage.getItem("groupValue");
     if (storedState) {
       return JSON.parse(storedState);
     }
-    return null; 
+    return null;
   }
 
   useEffect(() => {
     saveStateToLocalStorage(groupValue);
     async function fetchData() {
-      const response = await axios.get('https://api.quicksell.co/v1/internal/frontend-assignment');
+      const response = await axios.get(
+        "https://tfyincvdrafxe7ut2ziwuhe5cm0xvsdu.lambda-url.ap-south-1.on.aws/ticketAndUsers"
+      );
       await refactorData(response);
-  
     }
     fetchData();
-    async function refactorData(response){
-      let ticketArray = []
-        if(response.status  === 200){
-          for(let i=0; i<response.data.tickets.length; i++){
-            for(let j=0; j<response.data.users.length; j++){
-              if(response.data.tickets[i].userId === response.data.users[j].id){
-                let ticketJson = {...response.data.tickets[i], userObj: response.data.users[j]}
-                ticketArray.push(ticketJson)
-              }
+    async function refactorData(response) {
+      let ticketArray = [];
+      let userArray = [];
+      if (response.status === 200) {
+        for (let i = 0; i < response.data.tickets.length; i++) {
+          for (let j = 0; j < response.data.users.length; j++) {
+            if (response.data.tickets[i].userId === response.data.users[j].id) {
+              let ticketJson = {
+                ...response.data.tickets[i],
+                userObj: response.data.users[j],
+              };
+              ticketArray.push(ticketJson);
             }
           }
         }
-      await setticketDetails(ticketArray)
-      orderDataByValue(ticketArray)
+        response.data.users.forEach((ele) => userArray.push(ele.name));
+      }
+      await setUserList(userArray);
+      await setticketDetails(ticketArray);
+      orderDataByValue(ticketArray);
     }
-    
-  }, [orderDataByValue, groupValue])
+  }, [orderDataByValue, groupValue]);
 
-  function handleGroupValue(value){
+  function handleGroupValue(value) {
     setgroupValue(value);
     console.log(value);
   }
 
-  function handleOrderValue(value){
+  function handleOrderValue(value) {
     setorderValue(value);
     console.log(value);
   }
-  
+
   return (
     <>
       <Navbar
@@ -94,48 +111,54 @@ function App() {
         <div className="board-details-list">
           {
             {
-              'status' : <>
-                {
-                  statusList.map((listItem) => {
-                    return(<List
-                      groupValue='status'
-                      orderValue={orderValue}
-                      listTitle={listItem}
-                      listIcon=''
-                      statusList={statusList}
-                      ticketDetails={ticketDetails}
-                    />)
-                  })
-                }
-              </>,
-              'user' : <>
-              {
-                userList.map((listItem) => {
-                  return(<List
-                    groupValue='user'
-                    orderValue={orderValue}
-                    listTitle={listItem}
-                    listIcon=''
-                    userList={userList}
-                    ticketDetails={ticketDetails}
-                  />)
-                })
-              }
-              </>,
-              'priority' : <>
-              {
-                priorityList.map((listItem) => {
-                  return(<List
-                    groupValue='priority'
-                    orderValue={orderValue}
-                    listTitle={listItem.priority}
-                    listIcon=''
-                    priorityList={priorityList}
-                    ticketDetails={ticketDetails}
-                  />)
-                })
-              }
-            </>
+              status: (
+                <>
+                  {statusList.map((listItem) => {
+                    return (
+                      <List
+                        groupValue="status"
+                        orderValue={orderValue}
+                        listTitle={listItem}
+                        listIcon=""
+                        statusList={statusList}
+                        ticketDetails={ticketDetails}
+                      />
+                    );
+                  })}
+                </>
+              ),
+              user: (
+                <>
+                  {userList.map((listItem) => {
+                    return (
+                      <List
+                        groupValue="user"
+                        orderValue={orderValue}
+                        listTitle={listItem}
+                        listIcon=""
+                        userList={userList}
+                        ticketDetails={ticketDetails}
+                      />
+                    );
+                  })}
+                </>
+              ),
+              priority: (
+                <>
+                  {priorityList.map((listItem) => {
+                    return (
+                      <List
+                        groupValue="priority"
+                        orderValue={orderValue}
+                        listTitle={listItem.priority}
+                        listIcon=""
+                        priorityList={priorityList}
+                        ticketDetails={ticketDetails}
+                      />
+                    );
+                  })}
+                </>
+              ),
             }[groupValue]
           }
         </div>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,69 +1,80 @@
-import React, {useState} from 'react'
-import filterIcon from '../../static/Images/Filter.svg'
-import downArrowIcon from '../../static/Images/DownArrow.svg'
-
-import './Navbar.css'
+import React, { useState } from "react";
+import filterIcon from "../../static/Images/Filter.svg";
+import downArrowIcon from "../../static/Images/DownArrow.svg";
+import "./Navbar.css";
 
 export default function Navbar(props) {
-    const [Filter, setFilter] = useState(false);
+  const [Filter, setFilter] = useState(false);
 
-    function handleDisplayFilter(e){
-        setFilter(!Filter);
-        if(e.target.value !== undefined){
-            props.handleGroupValue(e.target.value);
-        }
+  function handleDisplayFilter(e) {
+    setFilter(!Filter);
+    if (e.target.value !== undefined) {
+      props.handleGroupValue(e.target.value);
     }
-    function handleOrderingValue(e){
-        setFilter(!Filter);
-        if(e.target.value !== undefined){
-            props.handleOrderValue(e.target.value);
-        }
+  }
+  function handleOrderingValue(e) {
+    setFilter(!Filter);
+    if (e.target.value !== undefined) {
+      props.handleOrderValue(e.target.value);
     }
+  }
 
   return (
     <>
-        <section className="nav">
-            <div className="nav_container">
-                <div>
-                    <div className="nav-disp-btn" onClick={handleDisplayFilter}>
-                        <div className="nav-disp-icon navr-disp-filter">
-                            <img src={filterIcon} alt="icon" />
-                        </div>
-                        <div className="nav-disp-heading">
-                            Display
-                        </div>
-                        <div className="nav-disp-icon nav-disp-drop">
-                            <img src={downArrowIcon} alt="icon" />
-                        </div>
-                    </div>
-                    <div className={Filter ? "nav-disp-dropdown nav-disp-dropdown-show" : "nav-disp-dropdown"}>
-                        <div className="nav-disp-filters">
-                            <div className="nav-dropdown-category">
-                                Grouping
-                            </div>
-                            <div className="nav-dropdown-selector">
-                                <select value={props.groupValue} onChange={handleDisplayFilter} className='nav-selector' name="grouping" id="">
-                                    <option value="status">Status</option>
-                                    <option value="user">User</option>
-                                    <option value="priority">Priority</option>
-                                </select>
-                            </div>
-                        </div>
-                        <div className="nav-disp-filters">
-                            <div className="nav-dropdown-category">
-                                Ordering
-                            </div>
-                            <div className="nav-dropdown-selector">
-                            <select value={props.orderValue} onChange={handleOrderingValue} className='nav-selector' name="grouping" id="">
-                                    <option value="priority">Priority</option>
-                                    <option value="title">Title</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+      <section className="nav">
+        <div className="nav_container">
+          <div style={{padding:"0.7rem 0.8rem"}}>
+            <div className="nav-disp-btn" onClick={handleDisplayFilter}>
+              <div className="nav-disp-icon navr-disp-filter">
+                <img src={filterIcon} alt="icon" />
+              </div>
+              <div className="nav-disp-heading">Display</div>
+              <div className="nav-disp-icon nav-disp-drop">
+                <img src={downArrowIcon} alt="icon" />
+              </div>
             </div>
-        </section>
+            <div
+              className={
+                Filter
+                  ? "nav-disp-dropdown nav-disp-dropdown-show"
+                  : "nav-disp-dropdown"
+              }
+            >
+              <div className="nav-disp-filters">
+                <div className="nav-dropdown-category">Grouping</div>
+                <div className="nav-dropdown-selector">
+                  <select
+                    value={props.groupValue}
+                    onChange={handleDisplayFilter}
+                    className="nav-selector"
+                    name="grouping"
+                    id=""
+                  >
+                    <option value="status">Status</option>
+                    <option value="user">User</option>
+                    <option value="priority">Priority</option>
+                  </select>
+                </div>
+              </div>
+              <div className="nav-disp-filters">
+                <div className="nav-dropdown-category">Ordering</div>
+                <div className="nav-dropdown-selector">
+                  <select
+                    value={props.orderValue}
+                    onChange={handleOrderingValue}
+                    className="nav-selector"
+                    name="grouping"
+                    id=""
+                  >
+                    <option value="priority">Priority</option>
+                    <option value="title">Title</option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
